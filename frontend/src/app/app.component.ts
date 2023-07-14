0b80e368ef62043fe5a972fbc97c6e10dbd95c9d
blancior <pkedziora347@gmail.com>
2023-07-15 00:12:46 +0200

    first concept of main page (header)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'frontend';
+  sounds:any[]=[
+    {
+      url: 'sound1.mp3',
+      nazwa: 'xd'
+    },
+    {
+      url: 'sound1.mp3',
+      nazwa: 'xd2'
+    },{
+      url: 'sound1.mp3',
+      nazwa: 'xd3'
+    }
+    ]
+  cisza:boolean=true;
 }
