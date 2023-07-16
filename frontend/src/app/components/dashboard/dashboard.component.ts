54227a6b086b8bcb04eb478f8b8a256dbae67ea7
blancior <pkedziora347@gmail.com>
2023-07-16 17:50:28 +0200

    added routing

---
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent {
+  sounds:any[]=[
+    {
+      url: 'sound1.mp3',
+      nazwa: 'lorem ipsum'
+    },
+    {
+      url: 'sound1.mp3',
+      nazwa: 'lorem ipsum2'
+    },{
+      url: 'sound1.mp3',
+      nazwa: 'lorem ipsum'
+    }
+  ]
+  isMuted:boolean=true;
+
+}
---
+++ frontend/src/app/components/landing-page/landing-page.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.css']
+})
+export class LandingPageComponent{
+
+
+}
