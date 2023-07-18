872012d7d96fcfe50f4504248c3440befde110d5
blancior <pkedziora347@gmail.com>
2023-07-18 23:32:44 +0200

    added landing page

--- frontend/src/app/components/landing-page/landing-page.component.ts
+++ frontend/src/app/components/landing-page/landing-page.component.ts
@@ -1,11 +1,21 @@
-import {Component} from '@angular/core';
+import {OnInit, Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.css']
+  styleUrls: ['./landing-page.component.scss']
 })
-export class LandingPageComponent{
-
+export class LandingPageComponent implements OnInit{
+  @ViewChild('imageElement') animationElement!: ElementRef;
+  isImageVisible:boolean = false;
+  imageSRC: string = './assets/pepe-pepejam-static.gif';
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.isImageVisible=true
+    },300);
+    setTimeout(() => {
+      this.imageSRC='./assets/pepe-pepejam-tr.gif'
+    },1500);
+  }
 
 }
