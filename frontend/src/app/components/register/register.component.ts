3fbdb1c26e471bdfd06a3a50e204980ecd5fee2a
blancior <pkedziora347@gmail.com>
2023-07-25 17:00:12 +0200

    created register component and its validation

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'register',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      email: ['',Validators.required,Validators.email],
+      username: ['',Validators.required,Validators.minLength(3),Validators.maxLength(32)],
+      password: ['',Validators.required,Validators.minLength(6),Validators.maxLength(32)],
+      confirmPassword: ['',Validators.required],
+    })
+  }
+
+  isSubmitted: boolean = false;
+  passwordsNotConfirmed?: boolean;
+  registerForm: FormGroup;
+
+
+  onSubmit(): void {
+    if(this.isSubmitted){
+      if (this.registerForm.value.password !== this.registerForm.value.confirmPassword) {
+        this.passwordsNotConfirmed = true;
+        console.log(this.registerForm.value.password, '=--=', this.registerForm.value.confirmPassword)
+        throw new Error('stop');
+      } else {
+        this.passwordsNotConfirmed = false;
+        console.log('Zarejestrowano użytkownika ', this.registerForm.value);
+      }
+    }
+    else throw new Error('niewypelniony formularz')
+  }
 }
