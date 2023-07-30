b0f57083563bbabfb7d4bd3b745a062285970602
blancior <pkedziora347@gmail.com>
2023-07-30 14:16:46 +0200

    implementation of songlist

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component} from '@angular/core';
+import { SongModel} from "../models/song.model";
+import axios from "axios";
 @Component({
   selector: 'dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent{
+  constructor() {
+    axios.get('http://localhost:4100/api/songs/all').then((response)=> {
+      console.log(response.data);
+      for (let i=0;i<=response.data.length;i++){
+        this.songs.push(response.data[i]);
+      }
+    })
+  }
+
+
+  songs: SongModel[]=[];
   sounds:any[]=[
     {
       url: 'sound1.mp3',
@@ -20,5 +32,7 @@ export class DashboardComponent {
     }
   ]
   isMuted:boolean=true;
+
+
 
 }
