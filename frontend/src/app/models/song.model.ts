40ef646b938b9c2d96f9aea0001369cba41d9f62
blancior <pkedziora347@gmail.com>
2023-08-06 19:31:23 +0200

    Feature: added Song model

--- frontend/src/app/models/song.model.ts
+++ frontend/src/app/models/song.model.ts
@@ -2,13 +2,14 @@ export interface SongModel{
   authorID: {
     _id:string,
     name:string,
-  }
+  },
+  thumbnail:string,
   categories:string[],
   likes:any[],
   name:string, //song name
   userID:{
     _id:string,
-    username:string;
+    username:string
   }, //id of person who added this song
   ytURL:string,
 }
