2abbd43639a6a28cdc1990bdd5d33d54b7b866c0
blancior <pkedziora347@gmail.com>
2023-08-06 22:48:40 +0200

    Feature: management panel

--- frontend/src/app/dashboard/management-panel/management-panel.component.ts
+++ frontend/src/app/dashboard/management-panel/management-panel.component.ts
@@ -1,6 +1,5 @@
-import {Component, Input} from '@angular/core';
+import {Component} from '@angular/core';
 import {DashboardComponent} from "../dashboard.component";
-import {SongFormComponent} from "./song-form/song-form.component";
 
 
 @Component({
