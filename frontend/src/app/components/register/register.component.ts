98f60c4bfc3babc2478b2f9db02c06d3679b0ea4
blancior <pkedziora347@gmail.com>
2023-08-13 13:43:33 +0200

    Feature: New user registration & form validation

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,38 +1,60 @@
-import { Component } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Component} from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators} from "@angular/forms";
+import axios from "axios";
 
 @Component({
   selector: 'register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent{
 
   constructor(private fb: FormBuilder) {
     this.registerForm = this.fb.group({
-      email: ['',Validators.required,Validators.email],
-      username: ['',Validators.required,Validators.minLength(3),Validators.maxLength(16)],
-      password: ['',Validators.required,Validators.minLength(6),Validators.maxLength(16)],
+      email: ['',[Validators.required,Validators.email]],
+      username: ['',[Validators.required,Validators.minLength(3)]],
+      password: ['',[Validators.required,Validators.minLength(5),Validators.maxLength(16)]],
       confirmPassword: ['',Validators.required],
-    })
+    },
+      {validator: this.matchValuesValidator('password', 'confirmPassword')}
+    )
   }
 
-  isSubmitted: boolean = false;
-  passwordsNotConfirmed?: boolean;
+
+  isValidated!: boolean;
+  passwordsNotConfirmed!: boolean;
   registerForm: FormGroup;
 
-
   onSubmit(): void {
-    if(this.isSubmitted){
-      if (this.registerForm.value.password !== this.registerForm.value.confirmPassword) {
-        this.passwordsNotConfirmed = true;
-        console.log(this.registerForm.value.password, '=--=', this.registerForm.value.confirmPassword)
-        throw new Error('stop');
-      } else {
-        this.passwordsNotConfirmed = false;
-        console.log('Zarejestrowano użytkownika ', this.registerForm.value);
-      }
+    this.checkForm();
+    if(this.isValidated){
+      throw new Error('niewypelniony formularz')
     }
-    else throw new Error('niewypelniony formularz')
+    else{
+      axios.post('http://localhost:4100/api/users/register',this.registerForm.value);
+      console.log('zarejestrowano uzytkownika', this.registerForm.value)
+    }
+  }
+
+   matchValuesValidator(controlName: string, matchingControlName: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const controlValue = control.get(controlName)?.value;
+      const matchingControlValue = control.get(matchingControlName)?.value;
+
+      if (!matchingControlValue) {
+        return null;
+      }
+
+      if (controlValue !== matchingControlValue) {
+        return { matchValues: true };
+      }
+
+      return null;
+    };
+  }
+  checkForm(){
+    this.isValidated = (this.registerForm.get('email')?.value == '' ||
+      this.registerForm.get('username')?.value == '' || this.registerForm.get('password')?.value == ''
+      || this.registerForm.get('confirmPassword')?.value == '');
   }
 }
