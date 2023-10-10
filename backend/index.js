954d11a535e8bc3313ed4c6667a693299133d46d
blancior <pkedziora347@gmail.com>
2023-10-10 14:56:30 +0200

    Update: CORS policy

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 //  <----------- Middlewares ----------->
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', 'https://discord-music-app-backend.vercel.app/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
