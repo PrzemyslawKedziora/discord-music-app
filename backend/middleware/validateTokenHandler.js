d8f7d7248416a71c0307df070561485cf7e7cf7d
blancior <pkedziora347@gmail.com>
2024-04-17 00:31:58 +0200

    Feat: fixed like system

--- backend/middleware/validateTokenHandler.js
+++ backend/middleware/validateTokenHandler.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 
 const validateToken = asyncHandler(async (req, res, next) => {
     let token;
-    let authHeader = req.headers.Authorization || req. headers.authorization;
+    let authHeader = req.headers.Authorization || req.headers.authorization;
     
     if(authHeader && authHeader.startsWith("Bearer")) {
         token = authHeader.split(" ")[1];
